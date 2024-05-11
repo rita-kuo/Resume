c6dcfb8f19ffd2c61bcc75b16afcc97d0676672c
rita-kuo-jedisoft <rita.kuo@jedisoft.com>
2024-05-12 04:56:32 +0800

    feat: collapse while scrolling

--- src/component/layout/Header.tsx
+++ src/component/layout/Header.tsx
@@ -1,11 +1,34 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 
 const Header: React.FC = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const translateY = `-${ref.current?.offsetHeight ?? 0}px`;
+
+  const [isScrolling, setIsScrolling] = useState<boolean>();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => setIsScrolling(true));
+    window.addEventListener("scrollend", () => setIsScrolling(false));
+    return () => {
+      window.removeEventListener("scroll", () => setIsScrolling(true));
+      window.removeEventListener("scrollend", () => setIsScrolling(false));
+    };
+  }, []);
+
   return (
-    <motion.header className="z-10 w-full bg-sky-100 p-4 shadow">
+    <motion.header
+      ref={ref}
+      initial={{
+        top: isScrolling || isScrolling === undefined ? 0 : translateY,
+      }}
+      animate={{
+        top: !isScrolling ? 0 : translateY,
+      }}
+      className="sticky top-0 z-10 w-full bg-sky-100 p-4 shadow"
+    >
       <div className="mx-auto flex w-full max-w-limit items-end justify-between">
         <h1 className="text-sky-800">Rita, the Engineer</h1>
       </div>
