94c2a1ec1f57b8ceb55ed174733079c981ab11d9
rita-kuo-jedisoft <rita.kuo@jedisoft.com>
2024-05-12 01:43:46 +0800

    feat: job header

--- src/component/career/history/JobHeader.tsx
+++ src/component/career/history/JobHeader.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from "react";
 import WorkHistory from "@/model/workHistory";
 import { format } from "date-fns";
+import { IoChevronDown } from "react-icons/io5";
 
 const JobHeader: React.FC<{ history: WorkHistory }> = (props) => {
   const from = useMemo(
@@ -13,9 +14,11 @@ const JobHeader: React.FC<{ history: WorkHistory }> = (props) => {
   );
 
   return (
-    <button className="group flex items-center gap-4">
-      <div className="h-5 w-5 rounded-full bg-blue-400 group-hover:bg-blue-300 md:h-6 md:w-6" />
-      <h3 className="space-x-4 text-sky-600 group-hover:text-sky-500 [&>*]:align-middle">
+    <button className="group flex items-center gap-4 text-sky-600 hover:text-sky-500">
+      <div className="flex h-5 w-5 items-center justify-center rounded-full bg-blue-400 text-white group-hover:bg-blue-300 md:h-6 md:w-6">
+        <IoChevronDown />
+      </div>
+      <h3 className="space-x-4 [&>*]:align-middle">
         <span className="text-sm font-medium">{`${from} - ${to}`}</span>
         <span>{props.history.company}</span>
       </h3>
