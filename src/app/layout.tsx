469f13ae20123dcd2cb895a39ec109305c489b19
rita-kuo-jedisoft <rita.kuo@jedisoft.com>
2024-05-12 07:10:12 +0800

    fix: update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`min-h-screen ${inter.className}`}>
         <Header />
-        <div className="mx-auto w-full max-w-limit items-start gap-10 md:flex">
+        <div className="mx-auto w-full max-w-limit items-start gap-10 px-4 md:flex">
           <aside className="sticky hidden min-h-max basis-1/5 space-y-4 md:block">
             <Information />
             <MenuContent />
