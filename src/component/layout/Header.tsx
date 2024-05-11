ad143eb2774008a1c5cea22ff45c3af1d5fe4c03
rita-kuo-jedisoft <rita.kuo@jedisoft.com>
2024-05-11 21:17:42 +0800

    feat: header

---
+++ src/component/layout/Header.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+const Header: React.FC = () => {
+  return (
+    <motion.header className="w-full bg-sky-100 p-4 shadow">
+      <div className="mx-auto flex w-full max-w-limit items-end justify-between">
+        <h1 className="text-sky-800">Rita, the Engineer</h1>
+      </div>
+    </motion.header>
+  );
+};
+
+export default Header;
