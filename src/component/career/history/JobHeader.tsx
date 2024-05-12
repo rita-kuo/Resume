b1fcfe0c84183bf91b1ba12be0775727078be851
Rita <tn.rita.kuo@gmail.com>
2024-05-12 13:02:51 +0800

    feat: update style
    Signed-off-by: Rita <tn.rita.kuo@gmail.com>

--- src/component/career/history/JobHeader.tsx
+++ src/component/career/history/JobHeader.tsx
@@ -1,91 +1,91 @@
-import React, { useMemo, useRef } from "react";
+import React, {useMemo, useRef} from "react";
 import WorkHistory from "@/model/workHistory";
-import { format } from "date-fns";
-import { motion } from "framer-motion";
-import { useWorkHistory } from "@/component/career/history/WorkHistoryContext";
-import { useCollapseStates } from "@/hook/useCollapseState";
-import { IoPlay } from "react-icons/io5";
+import {format} from "date-fns";
+import {motion} from "framer-motion";
+import {useWorkHistory} from "@/component/career/history/WorkHistoryContext";
+import {useCollapseStates} from "@/hook/useCollapseState";
+import {IoPlay} from "react-icons/io5";
 
 const dotSize = "h-5 w-5 md:h-6 md:w-6";
 const defaultColor = "#bfdbfe";
-const activeColor = "#fb7185";
+const activeColor = "#2dd4bf";
 
 const JobHeader: React.FC<{ history: WorkHistory }> = (props) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const { state, motionProps, open, close } = useCollapseStates({
-    initialMotionProps: {
-      dot: { initial: { backgroundColor: defaultColor } },
-      text: { initial: { color: defaultColor } },
-    },
-    renderMotionProps: (state) => ({
-      dot: {
-        initial:
-          state === "opening"
-            ? { rotate: 0, backgroundColor: defaultColor }
-            : { rotate: 90, backgroundColor: activeColor },
-        animate:
-          state === "opening"
-            ? { rotate: 90, backgroundColor: activeColor }
-            : { rotate: 0, backgroundColor: defaultColor },
-        onAnimationComplete: () =>
-          state === "opening" &&
-          ref.current?.scrollIntoView({ behavior: "smooth", block: "center" }),
-      },
-      text: {
-        initial:
-          state === "opening"
-            ? { color: defaultColor }
-            : { color: activeColor },
-        animate:
-          state === "opening"
-            ? { color: activeColor }
-            : { color: defaultColor },
-      },
-    }),
-  });
+    const ref = useRef<HTMLDivElement>(null);
+    const {state, motionProps, open, close} = useCollapseStates({
+        initialMotionProps: {
+            dot: {initial: {backgroundColor: defaultColor}},
+            text: {initial: {color: defaultColor}},
+        },
+        renderMotionProps: (state) => ({
+            dot: {
+                initial:
+                    state === "opening"
+                        ? {rotate: 0, backgroundColor: defaultColor}
+                        : {rotate: 90, backgroundColor: activeColor},
+                animate:
+                    state === "opening"
+                        ? {rotate: 90, backgroundColor: activeColor}
+                        : {rotate: 0, backgroundColor: defaultColor},
+                onAnimationComplete: () =>
+                    state === "opening" &&
+                    ref.current?.scrollIntoView({behavior: "smooth", block: "center"}),
+            },
+            text: {
+                initial:
+                    state === "opening"
+                        ? {color: defaultColor}
+                        : {color: activeColor},
+                animate:
+                    state === "opening"
+                        ? {color: activeColor}
+                        : {color: defaultColor},
+            },
+        }),
+    });
 
-  const from = useMemo(
-    () => format(props.history.from, "yyyy/MM"),
-    [props.history.from],
-  );
-  const to = useMemo(
-    () => (props.history.to ? format(props.history.to, "yyyy/MM") : "Present"),
-    [props.history.to],
-  );
+    const from = useMemo(
+        () => format(props.history.from, "yyyy/MM"),
+        [props.history.from],
+    );
+    const to = useMemo(
+        () => (props.history.to ? format(props.history.to, "yyyy/MM") : "Present"),
+        [props.history.to],
+    );
 
-  const { setCurrentId } = useWorkHistory({
-    historyId: props.history.id,
-    state,
-    open,
-    close,
-  });
+    const {setCurrentId} = useWorkHistory({
+        historyId: props.history.id,
+        state,
+        open,
+        close,
+    });
 
-  return (
-    <button
-      className="group flex items-end gap-4"
-      onClick={() =>
-        setCurrentId((current) =>
-          current === props.history.id ? undefined : props.history.id,
-        )
-      }
-    >
-      <motion.div
-        ref={ref}
-        {...motionProps?.dot}
-        className={`flex items-center justify-center rounded-full text-white ${dotSize} ${state === "closed" ? "group-hover:!bg-blue-300" : ""}`}
-      >
-        <IoPlay className="h-3 w-3 md:h-4 md:w-4" />
-      </motion.div>
+    return (
+        <button
+            className="group flex items-end gap-4"
+            onClick={() =>
+                setCurrentId((current) =>
+                    current === props.history.id ? undefined : props.history.id,
+                )
+            }
+        >
+            <motion.div
+                ref={ref}
+                {...motionProps?.dot}
+                className={`flex items-center justify-center rounded-full text-white ${dotSize} ${state === "closed" ? "group-hover:!bg-blue-300" : ""}`}
+            >
+                <IoPlay className="h-3 w-3 md:h-4 md:w-4"/>
+            </motion.div>
 
-      <motion.h3
-        {...motionProps?.text}
-        className={`space-x-2 [&>*]:align-baseline ${state === "closed" ? "group-hover:!text-blue-300" : ""}`}
-      >
-        <span className="text-sm font-medium">{`${from} - ${to}`}</span>
-        <span>{props.history.company}</span>
-      </motion.h3>
-    </button>
-  );
+            <motion.h3
+                {...motionProps?.text}
+                className={`space-x-2 [&>*]:align-baseline ${state === "closed" ? "group-hover:!text-blue-300" : ""}`}
+            >
+                <span className="text-sm font-medium">{`${from} - ${to}`}</span>
+                <span>{props.history.company}</span>
+            </motion.h3>
+        </button>
+    );
 };
 
 export default JobHeader;
--- src/component/layout/MenuContent.tsx
+++ src/component/layout/MenuContent.tsx
@@ -1,34 +1,34 @@
 import React from "react";
 import TextLink from "@/component/TextLink";
-import { useTranslations } from "next-intl";
-import { usePathname } from "@/navigation";
-import { BiCheck } from "react-icons/bi";
+import {useTranslations} from "next-intl";
+import {usePathname} from "@/navigation";
+import {BiCheck} from "react-icons/bi";
 
 const items = ["work-history", "education", "skills"];
 const MenuContent: React.FC<{ onClick?: () => void }> = (props) => {
-  const currentPathname = usePathname();
-  const t = useTranslations("Title");
-  return (
-    <div className="grid grid-cols-[max-content_1fr] gap-x-2 gap-y-6">
-      {items.map((pathname) => {
-        const isCurrent = currentPathname.startsWith(`/${pathname}`);
-        return isCurrent ? (
-          <>
-            <BiCheck className="h-6 w-6 text-rose-400" />
-            <span className="cursor-default font-bold text-rose-400">
+    const currentPathname = usePathname();
+    const t = useTranslations("Title");
+    return (
+        <div className="grid grid-cols-[max-content_1fr] gap-x-2 gap-y-6">
+            {items.map((pathname) => {
+                const isCurrent = currentPathname.startsWith(`/${pathname}`);
+                return isCurrent ? (
+                    <>
+                        <BiCheck className="h-6 w-6 text-teal-400"/>
+                        <span className="cursor-default font-bold text-teal-400">
               {t(pathname)}
             </span>
-          </>
-        ) : (
-          <div key={pathname} className="col-start-2 col-end-3">
-            <TextLink href={`/${pathname}`} onClick={props.onClick}>
-              {t(pathname)}
-            </TextLink>
-          </div>
-        );
-      })}
-    </div>
-  );
+                    </>
+                ) : (
+                    <div key={pathname} className="col-start-2 col-end-3">
+                        <TextLink href={`/${pathname}`} onClick={props.onClick}>
+                            {t(pathname)}
+                        </TextLink>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default MenuContent;
