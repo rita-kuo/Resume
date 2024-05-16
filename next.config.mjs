6c24ad330e16001d64d7ab847f680e3817f7ada7
Rita <tn.rita.kuo@gmail.com>
2024-05-16 16:34:03 +0800

    feat: deploy workflow
    Signed-off-by: Rita <tn.rita.kuo@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: 'resume',
+    basePath: '/resume',
     output: 'standalone',
 };
 
