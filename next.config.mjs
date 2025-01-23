20cd69ae1f88baea5e742700a8c71b77aeffb628
Rita <tn.rita.kuo@gmail.com>
2025-01-23 11:31:26 +0800

    feat: redirect origin resume path

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,16 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  redirects: async () => {
+    return [
+      {
+        source: '/resume/:locale/:path*',
+        destination: '/:locale/resume/:path*',
+        permanent: false
+      }
+    ];
+  }
 };
 
 export default withNextIntl(nextConfig);
